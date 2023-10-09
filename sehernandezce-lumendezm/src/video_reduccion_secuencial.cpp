25d91f9771e0771e161c8dcd0d73669749faae65
Sebastian <sehernandezce@unal.edu.co>
2023-10-09 03:34:26 -0500

    Video compresion secuencial version 1

--- sehernandezce-lumendezm/src/video_reduccion_secuencial.cpp
+++ sehernandezce-lumendezm/src/video_reduccion_secuencial.cpp
@@ -1,17 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[])
 {   
     // Lectura de parametros
-    char *archivoLectura = argv[1];
-    char *archivoEscritura = argv[2];
+    char *archive_read = argv[1];
+    char *archive_write = argv[2];
 
-    printf("Archivo de lectura: %s\n", archivoLectura);
-    printf("Archivo de escritura: %s\n", archivoEscritura);
+    // Abre el archivo de video original
+    cv::VideoCapture inputVideo(archive_read);
 
-    printf("version: %d.%d.%d\n", CV_MAJOR_VERSION, CV_MINOR_VERSION, CV_SUBMINOR_VERSION);
+    // Verifica si se pudo abrir el archivo de video
+    if (!inputVideo.isOpened()) {
+        std::cerr << "Error al abrir el archivo de video original." << std::endl;
+        return -1;
+    }
+
+    // Obtiene las propiedades del video original (ancho, alto, FPS)
+    double fps = inputVideo.get(cv::CAP_PROP_FPS);
+
+    // Ancho y alto del video objetivo
+    int redux = 3;
+    int frame_width = 640;
+    int frame_height = 360;
+
+    // Crea el objeto para escribir el nuevo video procesado
+    cv::VideoWriter outputVideo(archive_write, cv::VideoWriter::fourcc('X', '2', '6', '4'), fps, cv::Size(frame_width,frame_height));
+
+    cv::Mat frame; // Variable para almacenar el frame capturado
+    cv::Mat new_frame(frame_height, frame_width, CV_8UC3, cv::Scalar(255,255,255));
+
+    // Captura un frame del video original
+    while (true) {
+        
+        inputVideo >> frame;
+
+        // Verifica si el frame se capturó correctamente
+        if (frame.empty()) {
+            std::cerr << "No se pudo capturar el frame." << std::endl;
+            break;
+        }
+
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+
+        // Procesar el frame
+        for(int row = 0; row < new_frame.rows; row++)
+        {
+            for(int col = 0; col < new_frame.cols; col++)
+            {   
+                int red, green, blue;
+                
+                blue = frame.at<cv::Vec3b>(row*redux, col*redux)[0];
+                green = frame.at<cv::Vec3b>(row*redux, col*redux)[1];
+                red = frame.at<cv::Vec3b>(row*redux, col*redux)[2];
+
+                for(int i = 1; i < redux; i++)
+                {
+                    for(int j = 1; j < redux; j++)
+                    {
+                        blue += frame.at<cv::Vec3b>(row*redux + i, col*redux + j)[0];
+                        green += frame.at<cv::Vec3b>(row*redux + i, col*redux + j)[1];
+                        red += frame.at<cv::Vec3b>(row*redux + i, col*redux + j)[2];
+                    }
+                }
+
+                new_frame.at<cv::Vec3b>(row, col)[0] = (blue+(redux*redux)-1)/(redux*redux);
+                new_frame.at<cv::Vec3b>(row, col)[1] = (green+(redux*redux)-1)/(redux*redux);
+                new_frame.at<cv::Vec3b>(row, col)[2] = (red+(redux*redux)-1)/(redux*redux);
+            }
+        }
+
+        // Escribe el frame procesado en el nuevo video
+        outputVideo << new_frame;
+
+        // Muestra el frame procesado
+        cv::imshow("Frame Procesado", new_frame);
+
+        // Espera un momento y verifica si se presionó la tecla 'q' para salir del bucle
+        if (cv::waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    // Libera los recursos
+    inputVideo.release();
+    // outputVideo.release();
+    cv::destroyAllWindows();
+
     return 0;
 }
 
